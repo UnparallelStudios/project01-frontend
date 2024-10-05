55dce0d330eddc2e8dfff0c947f170847bc7895d
prathik-p <prathik.prejith@gmail.com>
2024-10-05 17:29:21 +0530

    create: base dashboard template

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import profImage from "./assets/pfpimage.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <Navbar />
+        <div className="content-container">
+          <div className="profile-feed-container">
+            {/* live feed container */}
+            <div className="feed-container"></div>
+            <div className="spacer-container"></div>
+            <div className="profile-container">
+              <img className="profImage" src={profImage}></img>
+            </div>
+          </div>
+          <div className="spacer-container2"></div>
+          {/* video translation container */}
+          <div className="trans-video-container"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
