c02114d27b9e3b28b13901ca8c77e37f463e950d
prathik-p <prathik.prejith@gmail.com>
2024-10-07 22:03:35 +0530

    create: login page with session control

---
+++ src/components/LoginPage/index.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./index.scss";
+import { useNavigate } from "react-router-dom";
+
+function LoginPage() {
+  const [usernameInput, setUsernameInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const navigate = useNavigate();
+
+  function handleChangeUsername(e) {
+    setUsernameInput(e.target.value);
+  }
+
+  function handleChangePassword(e) {
+    setPasswordInput(e.target.value);
+  }
+
+  function validateLogin() {
+    if (usernameInput == "admin" && passwordInput == "admin") {
+      localStorage.setItem("user", usernameInput);
+      alert("Success!");
+      navigate("/dash");
+    } else {
+      alert("Login failed!");
+      // navigate("/")
+    }
+  }
+
+  return (
+    <>
+      <div className="loginpage-container">
+        <div className="login-container">
+          <div className="login-image-container"></div>
+          <div className="login-divider"></div>
+          <div className="login-input-container">
+            <div className="login-header">Sign in</div>
+            <form className="user--pass--container" action="">
+              <div className="input-placeholder">Username</div>
+              <input
+                className="username-input"
+                onChange={handleChangeUsername}
+              ></input>
+              <div className="input-spacer" style={{ height: "10px" }}></div>
+              <div className="input-placeholder">Password</div>
+              <input
+                className="password-input"
+                onChange={handleChangePassword}
+              ></input>
+              <div className="input-spacer" style={{ height: "20px" }}></div>
+              <button className="auth-button" onClick={validateLogin}>
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import App from "./App";
+import LoginPage from "./components/LoginPage";
 import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="dash" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
